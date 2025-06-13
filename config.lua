981fd59cbbfded47c105b0054dabb53037ed35cf
DenniseWest <Elena231231b@email.cz>
2025-06-13 14:39:01 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,8 @@
+
+Config = {}
+
+-- Jazykové nastavení (použij: "cs", "en", "es", "de", "fr", "ru", "zh", "ar", "pt")
+Config.Language = "en"
+
+-- TTS (Text-to-Speech) přehrávání zprávy znakového překladu
+Config.TTS_Enabled = true
