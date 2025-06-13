9dfba6108a9accd7f6fea43cd07dbb4128fc12f5
DenniseWest <Elena231231b@email.cz>
2025-06-13 15:53:51 +0200

    Delete config.lua

--- config.lua
+++
@@ -1,8 +0,0 @@
-
-Config = {}
-
--- Jazykové nastavení (použij: "cs", "en", "es", "de", "fr", "ru", "zh", "ar", "pt")
-Config.Language = "en"
-
--- TTS (Text-to-Speech) přehrávání zprávy znakového překladu
-Config.TTS_Enabled = true
